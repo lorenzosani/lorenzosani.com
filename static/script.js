902c6e87c95df32db49c15ec9f1ebcb4e64cca19
lorexsan <lorenzosani97@gmail.com>
2019-12-04 11:41:21 +0000

    Add navbar

--- static/script.js
+++ static/script.js
@@ -1,7 +1,21 @@
 $(document).ready(() => {
+  // Title animation
   $("#title").css("opacity", "1");
+  $(".navigation").css("opacity", "1");
+  
 
-  //Enable fullpage.js
+  // Navbar toggle
+  // Toggle open and close nav styles on click
+  $('#nav-toggle').click(function() {
+    $('nav ul').slideToggle();
+  });
+  // Hamburger to X toggle
+  $('#nav-toggle').on('click', function() {
+    this.classList.toggle('active');
+  });
+
+
+  // Enable fullpage.js
   $("#fullpage").fullpage({
     licenseKey: "6565F2B8-BA0F41C3-BC8C06A0-1D42DA81",
     anchors: ["main", "about", "projects", "contact"],
