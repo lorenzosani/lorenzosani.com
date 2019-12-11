3ce45353ab6f2bcfa813c76325d6e27853843f16
lorexsan <lorenzosani97@gmail.com>
2019-12-11 19:20:07 +0000

    Add scroll animation on homepage

--- static/script.js
+++ static/script.js
@@ -2,24 +2,38 @@ $(document).ready(() => {
   // Title animation
   $("#title").css("opacity", "1");
   $(".navigation").css("opacity", "1");
-  
 
   // Navbar toggle
   // Toggle open and close nav styles on click
-  $('#nav-toggle').click(function() {
-    $('nav ul').slideToggle();
+  $("#nav-toggle").click(function() {
+    $("nav ul").slideToggle();
   });
-  $('nav ul li a').click(function() {
-    if ($('body').width() <= 769) {
-      $('#nav-toggle').removeClass('active');
-      $('nav ul').slideToggle();
+  $("nav ul li a").click(function() {
+    if ($("body").width() <= 769) {
+      $("#nav-toggle").removeClass("active");
+      $("nav ul").slideToggle();
     }
   });
-  // Hamburger to X toggle
-  $('#nav-toggle').on('click', function() {
-    this.classList.toggle('active');
+  $("#nav-toggle").on("click", function() {
+    this.classList.toggle("active");
   });
 
+  // Homepage scroll animation
+  animateHome = (e, currentPage) => {
+    if (e.originalEvent.wheelDelta < 0 && currentPage == 'home') {
+      $('#title').addClass('disappear');
+      if ($("body").width() > 769) {
+        $('.navigation').addClass('disappear');
+      }
+    } else if (e.originalEvent.wheelDelta > 0 && currentPage == 'about') {
+      $('#title').removeClass('disappear');
+      if ($("body").width() > 769) {
+        $('.navigation').removeClass('disappear');
+      }
+    }
+  }
+  $("#home").bind("mousewheel", (e) => { animateHome(e, 'home') });
+  $("#about-section").bind("mousewheel", (e) => { animateHome(e, 'about') });
 
   // Enable fullpage.js
   $("#fullpage").fullpage({
