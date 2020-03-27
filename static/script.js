d878f54bf23c5a072ee5cd2f9bf2b16a2ec4c2c1
lorexsan <lorenzosani97@gmail.com>
2020-03-27 22:51:50 +0000

    Add mobile swipe

--- static/script.js
+++ static/script.js
@@ -1,15 +1,21 @@
 $(document).ready(() => {
 
-  let sections = ["main", "about", "projects", "contact"]
+  let sections = ["main", "about", "projects", "contact"];
 
   let d = new Date();
   let lastScroll = d.getTime();
 
   // Hide splash screen and animate appearance
-  setTimeout(() => { $(".center-page").css("opacity", "100").css("top", "-10vh") }, 500);
+  setTimeout(() => {
+    $(".center-page")
+      .css("opacity", "100")
+      .css("top", "-10vh");
+  }, 500);
   $("#loader").css("opacity", "0");
   $("#loader").css("display", "none");
-  setTimeout(() => { $(".navigation, #scroll a span").css("opacity", "100") }, 2000);
+  setTimeout(() => {
+    $(".navigation, #scroll a span").css("opacity", "100");
+  }, 2000);
 
   // Navbar toggle open and close nav styles on click
   let nav = $(".nav-toggle");
@@ -23,57 +29,69 @@ $(document).ready(() => {
   });
   $(".nav-list li a").click(() => {
     if ($("body").width() <= 769) {
+      nav.removeClass("active");
       $("nav ul").slideToggle();
-      $(".nav-toggle").removeClass("active");
     }
   });
 
   // Sections appear and disappear animation
   homeDisappear = () => {
-      $("#home").addClass("disappearBg");
-      $("#header, .navigation").addClass("disappear");
+    $("#home").addClass("disappearBg");
+    $("#header, .navigation").addClass("disappear");
   };
-  homeAppear = () => {    
+  homeAppear = () => {
     $("#home").removeClass("disappearBg");
     setTimeout(() => {
       $("#header, .navigation").removeClass("disappear");
     }, 100);
   };
   aboutAppear = () => {
-    setTimeout(() => { $(".image #front").css("bottom", 0) }, 600);
-    setTimeout(() => { $("#nav-about").css("bottom", "50px").css("opacity", 1); }, 1200);
-  }
+    setTimeout(() => {
+      $(".image #front").css("bottom", 0);
+    }, 600);
+    setTimeout(() => {
+      $("#nav-about")
+        .css("bottom", "50px")
+        .css("opacity", 1);
+    }, 1200);
+  };
   aboutDisappear = () => {
     $(".image #front").css("bottom", "-80px");
-    $("#nav-about").css("bottom", "25px").css("opacity", 0);
-  }
+    $("#nav-about")
+      .css("bottom", "25px")
+      .css("opacity", 0);
+  };
   projectsAppear = () => {
-    setTimeout(() => { $(".image #front").css("bottom", 0) }, 600);
-  }
+    setTimeout(() => {
+      $(".image #front").css("bottom", 0);
+    }, 600);
+  };
   projectsDisappear = () => {
     $(".image #front").css("bottom", "-80px");
-  }
+  };
 
   // Mouse wheel scroll animation
-  $("#home, #about-section, #projects-section, #contact-section").bind("mousewheel", e => {
-    let d = new Date();
-    if (d.getTime()-lastScroll > 1000){
-      getScroll(e, fullpage_api.getActiveSection());
+  $("#home, #about-section, #projects-section, #contact-section").bind(
+    "mousewheel",
+    e => {
       let d = new Date();
-      lastScroll = d.getTime();
+      if (d.getTime() - lastScroll > 1000) {
+        getScroll(e, fullpage_api.getActiveSection());
+        let d = new Date();
+        lastScroll = d.getTime();
+      }
     }
-  });
+  );
   getScroll = (e, from) => {
     if (e.originalEvent.wheelDelta < 0) {
-      animateSection(sections[from["index"]], sections[from["index"]+1]);
+      animateSection(sections[from["index"]], sections[from["index"] + 1]);
     } else if (e.originalEvent.wheelDelta > 0) {
-      animateSection(sections[from["index"]], sections[from["index"]-1]);
+      animateSection(sections[from["index"]], sections[from["index"] - 1]);
     }
-  }
-
+  };
+  
   // Animates transitions from one section to other
   animateSection = (from, to) => {
-
     // Home animation
     if (from == "main") {
       homeDisappear();
@@ -89,13 +107,13 @@ $(document).ready(() => {
     }
 
     // Projects animation
-    if(to == "projects") {
+    if (to == "projects") {
       projectsAppear();
     } else if (from == "projects") {
       projectsDisappear();
     }
     fullpage_api.moveTo(to, 0);
-  }  
+  };
 
   // Enable fullpage.js
   $("#fullpage").fullpage({
